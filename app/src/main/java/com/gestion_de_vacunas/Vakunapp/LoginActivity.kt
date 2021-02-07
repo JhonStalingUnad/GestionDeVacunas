24581e83997819a606c6c1e304d167afd4222e42
davidsuarezunad <davidsuareztasama@gmail.com>
2021-02-06 22:57:57 -0500

    se crea menu y se da accion al boton continuar

--- app/src/main/java/com/gestion_de_vacunas/Vakunapp/LoginActivity.kt
+++ app/src/main/java/com/gestion_de_vacunas/Vakunapp/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.gestion_de_vacunas.Vakunapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin: Button = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener {
+            val intent = Intent(this,MenuActivity::class.java)
+            startActivity(intent);
         }
+    }
+
 }
