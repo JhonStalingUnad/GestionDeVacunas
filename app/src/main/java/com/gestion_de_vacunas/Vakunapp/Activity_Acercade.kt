0a23cd041668942839d6583a890ee9c44cbd5344
Alba Florez <76764239+albaf1804@users.noreply.github.com>
2021-03-07 01:50:09 -0500

    se crea Activity Acerca de

---
+++ app/src/main/java/com/gestion_de_vacunas/Vakunapp/Activity_Acercade.kt
@@ -0,0 +1,11 @@
+package com.gestion_de_vacunas.Vakunapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Acercade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__acercade)
+    }
+}
