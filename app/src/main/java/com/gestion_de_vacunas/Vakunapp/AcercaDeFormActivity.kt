e1c95af0f5c9a620947360c7d95ed3637df5e283
Jhon Stalling Sevillano Zemanate <jhon.sevillano@seratic.com>
2021-03-12 13:31:14 -0500

    Se realiza ajuste sobre la pantalla del Acerca De

--- app/src/main/java/com/gestion_de_vacunas/Vakunapp/AcercaDeFormActivity.kt
+++ app/src/main/java/com/gestion_de_vacunas/Vakunapp/AcercaDeFormActivity.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 class AcercaDeFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity__acercade)
+        setContentView(R.layout.activity_form_acerca_de)
     }
 
     override fun onBackPressed() {
